12f08b929d140d5169e9ed6e78cfb22df488a6b5
Benrico Krog <benricok@krog.dev>
2024-02-19 13:41:54 +0200

    init tut2

---
+++ starter/intro.py
@@ -0,0 +1,10 @@
+import stdio
+import sys
+
+def main():
+    # Write 'Hello, World' to standard output.
+    stdio.write('Hello, World\n')
+    stdio.writeln(sys.argv[0])
+
+if __name__ == '__main__':
+    main()
---
+++ tut2/1.py
@@ -0,0 +1,8 @@
+import sys
+import stdio
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main() 
